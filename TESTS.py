5257a22b02150d82358a4182388680a12658c1cf
Aamirul57 <91449633+Aamirul57@users.noreply.github.com>
2024-11-18 04:22:38 +0800

    streamlit testing

--- TESTS.py
+++ TESTS.py
@@ -235,7 +235,7 @@ label_dict = {
 }
 
 # Load the pre-trained model
-model = load_model("AisyahSignX100.keras")  # Replace with your actual model path
+model = load_model("https://github.com/Aamirul57/StreamlitASL/blob/main/AisyahSignX100.keras")  # Replace with your actual model path
 
 def sign_detection():
     st.subheader("Real-time Sign Detection")
@@ -313,29 +313,29 @@ def sign_detection():
 
 #     cap.release()
 
-import cv2
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+    import cv2
+    from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-faceCascade = cv2.CascadeClassifier(cv2.haarcascades+'haarcascade_frontalface_default.xml')
+    faceCascade = cv2.CascadeClassifier(cv2.haarcascades+'haarcascade_frontalface_default.xml')
 
 
-class VideoTransformer(VideoTransformerBase):
-    def init(self):
-        self.i = 0
+    class VideoTransformer(VideoTransformerBase):
+        def init(self):
+            self.i = 0
 
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-        i =self.i+1
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (95, 207, 30), 3)
-            cv2.rectangle(img, (x, y - 40), (x + w, y), (95, 207, 30), -1)
-            cv2.putText(img, 'F-' + str(i), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+        def transform(self, frame):
+            img = frame.to_ndarray(format="bgr24")
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+            i =self.i+1
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (95, 207, 30), 3)
+                cv2.rectangle(img, (x, y - 40), (x + w, y), (95, 207, 30), -1)
+                cv2.putText(img, 'F-' + str(i), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
 
-        return img
+            return img
 
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+    webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
 
 
 # Quiz feature
